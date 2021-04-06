997ab16e9556195ec534c7fd7b7002c7a5d73a9e
fallying <jonasexplore@gmail.com>
2021-04-06 16:56:07 -0300

    create routes

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { KeyModule } from '../keys/key.module';
+import { UserModule } from '../users/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionModule } from '../transactions/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, KeyModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
