324dee61e4df04a7854e32eae7a290d5b6b55ef5
fallying <jonasexplore@gmail.com>
2021-04-08 15:07:02 -0300

    add env port to heruko

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from './modules/app/app.module';
 import { configSwagger } from './config/SwaggerConfig';
 
 async function bootstrap() {
+  const port: number = parseInt(`${process.env.PORT}`) || 3000;
   const app = await NestFactory.create(AppModule);
 
   const document = SwaggerModule.createDocument(app, configSwagger);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
