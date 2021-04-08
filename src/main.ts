70d9e7be0a6e544225c3b2dcaa5e3c5272eb1013
fallying <jonasexplore@gmail.com>
2021-04-08 15:27:11 -0300

    deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { AppModule } from './modules/app/app.module';
 import { configSwagger } from './config/SwaggerConfig';
 
 async function bootstrap() {
-  const port: number = parseInt(`${process.env.PORT}`) || 3000;
   const app = await NestFactory.create(AppModule);
 
   const document = SwaggerModule.createDocument(app, configSwagger);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(port);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
